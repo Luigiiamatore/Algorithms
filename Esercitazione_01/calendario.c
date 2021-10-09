c3f690fcd9d3d2431b64e9e82097e7d9b35c6fcf
Luigi Amatore <luigi.amatore7@gmail.com>
2021-10-10 01:11:25 +0200

    Update rep

--- Esercitazione_01/calendario.c
+++ Esercitazione_01/calendario.c
@@ -7,15 +7,61 @@ int main(void) {
     printf("Inserisci il giorno di inizio mese (1 = lunedì, ..., 7 = domenica): ");
     scanf("%d", &giorno);
 
-    printf(" L   M   M   G   V   S   D\n");
+    giorno--;
+    int ultimoGiorno;
 
     switch(mese) {
         case 2:
-            
+            ultimoGiorno = 28;
+            break;
         case 4:
         case 6:
         case 9:
         case 11:
+            ultimoGiorno = 30;
+            break;
         default:
+            ultimoGiorno = 31;
     }
+
+    int cal[5][7];
+    int contatore = 1;
+
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 7; j++) {
+            if(i == 0 && j < giorno)
+                cal[i][j] = 0;
+            else if(contatore <= ultimoGiorno){
+                cal[i][j] = contatore;
+                contatore++;
+            }
+            else
+                cal[i][j] = 0;
+        }
+    }
+
+    /*for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 7; j++){
+            printf("%d   ", cal[i][j]);
+        }
+        printf("\n");
+    }*/
+
+    printf(" L   M   M   G   V   S   D\n ");
+    for(int i = 0; i < 5; i ++) {
+        for(int j = 0; j < 7; j++) {
+            if(cal[i][j] == 0)
+                printf(" ");
+            else
+                printf("%d", cal[i][j]);
+            
+            if(cal[i][j] >= 10){
+                printf("  ");
+            } else {
+                printf("   ");
+            }
+        }
+        printf("\n ");
+    }
+    return 0;
 }
