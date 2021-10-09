0c357566d8cda3cc68416505f49c45f4316a5b62
Luigi Amatore <luigi.amatore7@gmail.com>
2021-10-09 17:52:29 +0200

    Local files

---
+++ Esercitazione_01/calendario.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    int mese, giorno;
+    printf("Inserisci il numero del mese (1 = gennaio, ..., 12 = dicembre): ");
+    scanf("%d", &mese);
+    printf("Inserisci il giorno di inizio mese (1 = lunedì, ..., 7 = domenica): ");
+    scanf("%d", &giorno);
+
+    printf(" L   M   M   G   V   S   D\n");
+
+    switch(mese) {
+        case 2:
+            
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+        default:
+    }
+}
